6a9feb32c163945787a9b36a0cf291765fe475fe
Daniela Javier <daniela_javier@dlsu.edu.ph>
2018-06-28 05:00:30 +0800

    integrated with gui pls debug backend nalang ty

--- MIPS.java
+++ MIPS.java
@@ -3,6 +3,7 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
+import javax.swing.JOptionPane;
 import javax.swing.JButton;
 import javax.swing.JTextArea;
 import java.awt.Window.Type;
@@ -38,38 +39,38 @@ import java.util.regex.Pattern;
 public class MIPS {
 	
 	//GUI Variables
-	JPanel panel;
-	JLabel lblNewLabel;
-	JTabbedPane tabbedPane;
-	JPanel panel_1;
-	JPanel panel_3;
-	JLabel lblEnterCodeBelow;
-	JButton button;
-	JScrollPane scrollPane_1;
-	JTextArea textArea;
-	JPanel panel_4;
-	JTabbedPane tabbedPane_1;
-	JPanel panel_6;
-	JScrollPane scrollPane_2;
-	JTextArea textArea_1;
-	JPanel panel_7;
-	JScrollPane scrollPane_3;
-	JTextArea textArea_2;
-	JPanel panel_2;
-	JTabbedPane tabbedPane_2;
-	JPanel panel_5;
-	JScrollPane scrollPane_4;
-	JTextPane txtpndataIbytebyte;
-	JPanel panel_8;
-	JScrollPane scrollPane;
-	JTextPane txtpnAvailableInstructions;
+	public static JPanel panel;
+	public static JLabel lblNewLabel;
+	public static JTabbedPane tabbedPane;
+	public static JPanel panel_1;
+	public static JPanel panel_3;
+	public static JLabel lblEnterCodeBelow;
+	public static JButton button;
+	public static JScrollPane scrollPane_1;
+	public static JTextArea textArea;
+	public static JPanel panel_4;
+	public static JTabbedPane tabbedPane_1;
+	public static JPanel panel_6;
+	public static JScrollPane scrollPane_2;
+	public static JTextArea textArea_1;
+	public static JPanel panel_7;
+	public static JScrollPane scrollPane_3;
+	public static JTextArea textArea_2;
+	public static JPanel panel_2;
+	public static JTabbedPane tabbedPane_2;
+	public static JPanel panel_5;
+	public static JScrollPane scrollPane_4;
+	public static JTextPane txtpndataIbytebyte;
+	public static JPanel panel_8;
+	public static JScrollPane scrollPane;
+	public static JTextPane txtpnAvailableInstructions;
 	private JFrame frmUmips;
 	
 	
 	
 	
 	//ErrorCheck Variables
-	String reg[][] = {
+	public static String reg[][] = {
 	        {"R0","0000000000000000"},{"R1","0000000000000000"},{"R2","0000000000000000"},{"R3","0000000000000000"},
 	        {"R4","0000000000000000"},{"R5","0000000000000000"},{"R6","0000000000000000"},{"R7","0000000000000000"},
 	        {"R8","0000000000000000"},{"R9","0000000000000000"},{"R10","0000000000000000"},{"R11","0000000000000000"},
@@ -80,26 +81,25 @@ public class MIPS {
 	        {"R28","0000000000000000"},{"R29","0000000000000000"},{"R30","0000000000000000"},{"R31","0000000000000000"},
 	        };
 	        
-	String ins[] = {"DADDIU","LD", "SD", "DADDU", "SLT", "NOP" , "BC", "BGEC", "DAUI"};
+	public static String ins[] = {"DADDIU","LD", "SD", "DADDU", "SLT", "NOP" , "BC", "BGEC", "DAUI"};
 	        
-	String inputCode = "DADDIU R1, R2, #0000";
+	public static String inputCode;
 	        
-	String[] breakCode = inputCode.split("(, )|( )|(,)"); //seperates the inputCode
-	int insError = 0; //use it nalang if 1 sha then error if 0 then no
-	int regError = 0; // if register error
-	int thirdError = 0; // if immError/reg3Error
+	public static String[] breakCode; //seperates the inputCode
+	public static int insError = 0; //use it nalang if 1 sha then error if 0 then no
+	public static int regError = 0; // if register error
+	public static int thirdError = 0; // if immError/reg3Error
 	
 	
 	
 	//Opcode Variables
-	String test = "DADDIU R3,R0,#0002";    
-    //String test = "LD R1,ALPHA(R0)";
-    String upper = null,
+	public static String test = "";    
+	public static String upper = null,
             lower = null,
             output = null;
     
-    int offset = 0, dec, low_int, up_int; 
-    String regop[][] ={
+    public static int offset = 0, dec, low_int, up_int; 
+    public static String regop[][] ={
                         {"R0","00000"},{"R1","00001"},{"R2","00010"},{"R3","00011"},
                         {"R4","00100"},{"R5","00101"},{"R6","00110"},{"R7","00111"},
                         {"R8","01000"},{"R9","01001"},{"R10","01010"},{"R11","01011"},
@@ -110,32 +110,22 @@ public class MIPS {
                         {"R28","0000000000000000"},{"R29","0000000000000000"},{"R30","0000000000000000"},{"R31","000000000000000X"},
                     };
     
-    String[] split = test.split(" ");
-    String[] split_1 = split[1].split(","); 
-    String instruction = split[0];   
-    int x =0;
-    String op_inst = null,RD = null,RS = null,RT = null, imm = null,base = null, off = "0001000000000000";
+    public static String[] split;
+    public static String[] split_1;
+    public static String instruction;   
+    public static int x =0;
+    public static String op_inst = null,RD = null,RS = null,RT = null, imm = null,base = null, off = "0001000000000000";
    
-    int size = split_1.length; 
-    String[] op_register = new String[size];
-    
-    for(int ctr=0; ctr < size; ctr++){
-        
-        for(int ctr_2=0; ctr_2 < 32; ctr_2++){
-       
-            if (split_1[ctr].equals(regop[ctr_2][0])){
-                op_register[x] = regop[ctr_2][1];
-                x++;
-            }
-        }
-     } 
-    String[] split_3 = null;
+    public static int size;
+    public static String[] op_register;
+    public static String[] split_3 = null;
 
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+	    	
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -199,11 +189,65 @@ public class MIPS {
 		lblEnterCodeBelow.setFont(new Font("Monospaced", Font.PLAIN, 16));
 		panel_3.add(lblEnterCodeBelow);
 		
-		button = new JButton("submit");
-		button.setBounds(95, 387, 99, 33);
+		button = new JButton("run");
+		button.setBounds(37, 388, 99, 33);
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				resetVar();
+				//badumtsssssss run here
+				String[] temp = textArea.getText().split("\n");
+				int error = 0;
+				int xd = temp.length;
+				int cnt = 0;
+				int errtype = 0; //1=insterror, 2=regerror, 3=immerror
 				
+				//check if may error
+				while(error == 0 && cnt != xd){
+					//set variable
+					error = errorCheck(temp[cnt])[0];
+					if(error == 1)
+						if(errorCheck(temp[cnt])[1] == 1)
+							errtype = 1;
+						else if(errorCheck(temp[cnt])[2] == 1)
+							errtype = 2;
+						else
+							errtype = 3;
+					else
+						cnt++;
+				}
+				
+				//if no error continue to opcode, if error, alert "ERROR!"
+				switch(error){
+					case 0: 
+						String henlo;
+						//loop for opcode of each inst and print
+						for(int q = 0; q < xd; q++){
+							henlo = wopCode(temp[q]);
+							textArea_2.setText(temp[q]+ ": " + textArea_2.getText() + henlo);
+						}	
+						break;
+						
+					case 1:
+						//alert error
+						JOptionPane.showMessageDialog(null, "Error! Please reset and try again.", "Error!", 1);
+						textArea_2.setText("Error!");
+						
+						//add errors to txt area 1.
+						switch(errtype){
+							case 1:
+								textArea_1.setText("_Syntax Error_ on line" + (cnt+1));
+								break;
+							case 2:
+								textArea_1.setText("_Register Error_ on line" + (cnt+1));
+								break;
+							case 3:
+								textArea_1.setText("_Third Parameter/Immediate Error_ on line" + (cnt+1));
+								break;
+							default:
+								break;
+						}
+						break;
+				}
 			}
 		});
 		
@@ -217,6 +261,17 @@ public class MIPS {
 		scrollPane_1.setViewportView(textArea);
 		button.setFont(new Font("Monospaced", Font.PLAIN, 18));
 		panel_3.add(button);
+		
+		JButton btnReset = new JButton("reset");
+		btnReset.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//reset method reset all important variables
+				reset();
+			}
+		});
+		btnReset.setFont(new Font("Monospaced", Font.PLAIN, 18));
+		btnReset.setBounds(151, 388, 99, 33);
+		panel_3.add(btnReset);
 		
 		panel_4 = new JPanel();
 		panel_4.setBounds(290, 0, 344, 421);
@@ -294,7 +349,278 @@ public class MIPS {
 		scrollPane.setViewportView(txtpnAvailableInstructions);
 	}
 	
+	public int[] errorCheck(String yes){
+		inputCode = yes;
+		breakCode = inputCode.split("(, )|( )|(,)");
+		int[] myArray = new int[4];
+		
+		 for(int i = 0; i < ins.length; i++)
+	        {
+	            if(breakCode[0].equals(ins[i])){
+	                insError = 0;
+	                break;
+	            }
+	            else{
+	                insError = 1;
+	            }
+	        }
+	        //seperate the instructions because these use a format of ins r1, r2, imm/r3
+	        if(breakCode[0].equals("DADDIU")||breakCode[0].equals("DADDU")||breakCode[0].equals("SLT")||breakCode[0].equals("DAUI")||breakCode[0].equals("BGEC"))
+	        {
+	            //if register 1 is valid
+	            for(int i = 0; i < ins.length; i++)
+	            {
+	                if(breakCode[1].equals(reg[i][0]))
+	                {
+	                    regError = 0;
+	                    break;
+	                }
+	                else{
+	                    regError = 1;
+	                }
+	            }
+
+	            //if register2 is valid
+	            for(int i = 0; i < ins.length; i++)
+	            {
+	                if(breakCode[2].equals(reg[i][0]))
+	                {
+	                    regError = 0;
+	                    break;
+	                }
+	                else{
+	                    regError = 1;
+	                }
+	            }
+	            
+	            //checks what type of parameter the 3rd one is
+	            if(breakCode[0].equals("DADDIU")||breakCode[0].equals("DAUI")) //commands with IMMEDIATE as their 3rd param
+	            {
+	                if(breakCode[3].charAt(0) == '#' && breakCode[3].length() ==  5) //checks if it starts with a # and is 5 chars long (# + hex)
+	                    thirdError = 0;
+	                else
+	                    thirdError = 1;
+	                   
+	            }
+	            
+	            else if(breakCode[0].equals("DADDU")||breakCode[0].equals("SLT")) //commands with 3RD REGISTER as their 3rd param
+	            {
+	                for(int i = 0; i < ins.length; i++)
+	                {
+	                    if(breakCode[3].equals(reg[i][0]))
+	                    {
+	                        thirdError = 0;
+	                        break;
+	                    }
+	                    else{
+	                        thirdError = 1;
+	                    }
+	                }
+	            }
+	            else { //BGEC
+	                for(int i = 0 ; i < reg.length; i++)
+	                {
+	                    if(breakCode[3].equals(reg[i][0]))
+	                    {
+	                        thirdError = 1;
+	                        break;
+	                    } 
+	                    else
+	                    {
+	                        thirdError = 0;
+	                    }
+	                }
+	                
+	                if(!breakCode[3].equals("[^A-Za-z0-9]"))
+	                {
+	                    thirdError = 1;
+	                }
+	            }
+	        }
+
+	        else if(breakCode[0].equals("LD")||breakCode[0].equals("SD")) //2 PARAMETER INSTRUCTIONS
+	        {
+	            //checks first parameter
+	            for(int i = 0; i < ins.length; i++)
+	            {
+	                if(breakCode[1].equals(reg[i][0]))
+	                {
+	                    regError = 0;
+	                    break;
+	                }
+	                else{
+	                    regError = 1;
+	                }
+	            }
+	            
+	            if(!breakCode[3].equals("[^A-Za-z0-9]"))
+	            {
+	                    thirdError = 1;
+	            }
+	        }
+	        
+	        else if(breakCode[0].equals("BC"))
+	        {
+	            if(breakCode[1].equals("[^A-Za-z0-9]"))
+	            {
+	                regError = 0;
+	            }
+	            else{
+	                regError = 1;
+	            }
+	 
+	        }
+	        
+	        else { //NOP instruction
+	            //checks 1st parameter
+	        }
+	        
+	        
+	        //returns if error and error typez
+	        if(insError == 0 && regError == 0 && thirdError == 0)
+	        	myArray[0] = 0;
+	        else
+	        	myArray[0] = 1;
+	        
+	        myArray[1] = insError;
+	        myArray[2] = regError;
+	        myArray[3] = thirdError;
+	        
+	        return myArray;
+	    }
 	
+	public String wopCode(String yes){
+		test = yes;
+		split = test.split(" ");
+        split_1 = split[1].split(","); 
+        instruction = split[0];   
+        x =0;
+        op_inst = null;
+        RD = null;
+        RS = null;
+        RT = null;
+        imm = null;
+        base = null;
+        off = "0001000000000000";
+       
+        size = split_1.length; 
+        op_register = new String[size];
+        
+        for(int ctr=0; ctr < size; ctr++){
+            
+            for(int ctr_2=0; ctr_2 < 32; ctr_2++){
+           
+                if (split_1[ctr].equals(regop[ctr_2][0])){
+                    op_register[x] = regop[ctr_2][1];
+                    x++;
+                }
+            }
+         } 
+        
+        split_3 = null;
+		
+		
+		switch(instruction){         
+        case "LD": op_inst = "110111"; break; //base, rt, offset*
+        case "SD": op_inst = "111111"; break; //base, rt, offset*
+        case "DADDIU": op_inst = "011001"; break;   //rs, rt, immediate*
+        case "SLT": op_inst = "000000"; break;   //rs, rt, ||rd, 00000, 101010*
+        case "BC": op_inst = "110010"; break;   //offset
+        case "BGEC": op_inst = "010110"; 
+                     off = "11110000000000000001"; break; //rs, rt, offset
+        case "DAUI": op_inst = "011101"; break; //rs, rt, immediate*  | rt rs
+    }
+    
+    if(instruction.equals("LD") || instruction.equals("SD")){
+        String[] split_2 = split_1[1].split("[()]");
+        System.out.println(split_2[0]);
+        for(int ctr=0; ctr < 32; ctr++){
+            if(split_2[1].equals(regop[ctr][0])){
+                base = regop[ctr][1];
+            }
+        }           
+        RT = op_register[0];
+        upper = op_inst + base + RT;
+        offset = Integer.parseInt(off, 2);
+        lower = Integer.toString(offset,16);   
+        dec = Integer.parseInt(upper,2);
+        output = Integer.toString(dec,16) + lower;
+    }
+    else if(instruction.equals("DADDIU") || instruction.equals("DAUI")){
+        split_3 = split_1[2].split("#");
+        RT = op_register[0];
+        RS = op_register[1];
+        imm = split_3[1];
+        upper = op_inst + RS + RT;
+        lower = imm;
+        //System.out.println(op_inst + " " + RS + " " + RT + " " + imm);
+        up_int = Integer.parseInt(upper,2);
+        //low_int = Integer.parseInt(lower,2);
+        
+        output = Integer.toString(up_int,16) + lower;
+    }
+    else if(instruction.equals("SLT")){
+        RD = op_register[0];
+        RS = op_register[1];
+        RT = op_register[2];
+        
+        upper = "1111" + op_inst + RS +  RT;
+        lower = RD + "00000101010";
+        up_int = Integer.parseInt(upper,2);
+        low_int = Integer.parseInt(lower,2);
+        output = Integer.toString(up_int,16).substring(1) + Integer.toString(low_int,16);
+    }
+    else if(instruction.equals("BGEC")){
+        RS = op_register[0];
+        RT = op_register[1];
+        upper = op_inst + RS + RT;
+        offset = Integer.parseInt(off, 2);
+        lower = Integer.toString(offset,16).substring(1); 
+        dec = Integer.parseInt(upper,2);
+        output = Integer.toString(dec,16) + lower;
+        
+    }
+    
+    return output;
+	}
 	
+	public void resetVar(){
+			textArea_1.setText("");
+			textArea_2.setText("");
+
+			inputCode = "";
+			breakCode = null;
+			insError = 0;
+			regError = 0;
+			thirdError = 0;
+
+			test = "";
+			upper = null;
+			lower = null;
+			output = null;
+			offset = 0;
+			dec = 0;
+			low_int = 0;
+			up_int = 0;
+			split = null;
+			split_1 = null;
+			instruction = null;
+			x=0;
+			op_inst = null;
+			RD = null;
+			RS = null;
+			RT = null;
+			imm = null;
+			base = null;
+			off = "0001000000000000";
+			size = 0;
+			op_register = null;
+			split_3 = null;
+	}
+	
+	public void reset(){
+		textArea.setText("");
+		resetVar();
+	}
 	
 }
